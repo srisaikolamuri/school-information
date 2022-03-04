82dec010bf9d9ae2e1f43ee129843a3be497ba9c
srisaikolamuri <srisaik.863@gmail.com>
2022-03-04 14:36:22 +0530

    school infromation

---
+++ src/com/java/Git_Test.java
@@ -0,0 +1,32 @@
+package com.java;
+
+public class Git_Test {
+	public void school() {
+		System.out.println("Bhashyam public school");
+	}
+
+	public void name() {
+		System.out.println("K sri sai govind");
+	}
+
+	public void schoolLocation() {
+		System.out.println("tanuku");
+	}
+
+	public void studying() {
+		System.out.println("9th clss");
+	}
+
+	public void section() {
+		System.out.println("G section");
+	}
+
+	public static void main(String[] args) {
+		Git_Test gt = new Git_Test();
+		gt.name();
+		gt.studying();
+		gt.section();
+		gt.school();
+		gt.schoolLocation();
+	}
+}
